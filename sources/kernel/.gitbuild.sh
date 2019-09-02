a9fc8cbc8e075790ba5bb98eb2d10962177100dd
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2019-09-01 21:40:43 -0300

    Sun 01 Sep 2019 09:32:37 PM -03

--- sources/kernel/.gitbuild.sh
+++ sources/kernel/.gitbuild.sh
@@ -7,11 +7,12 @@ MAINDIR=$(dirname $0)
 [ "$MAINDIR" == "." ] && MAINDIR=$(pwd)
 
 NUMCPU=`cat /proc/cpuinfo | grep proce | wc -l`
-NCPU=$(($NUMCPU + 2))
+#NCPU=$(($NUMCPU + 2))
+NCPU=$(($NUMCPU))
 #NCPU=1
 
 MYARCH="amd64"  # (amd64|x86|arm64|armhf|armel)
-TOARCH="amd64"  # (amd64|x86|arm64|armhf|armel)
+TOARCH="arm64"  # (amd64|x86|arm64|armhf|armel)
 
 KCROSS=0        # are you cross compiling ? (automatic)
 if [ "$MYARCH" != "$TOARCH" ]; then
@@ -19,19 +20,19 @@ if [ "$MYARCH" != "$TOARCH" ]; then
 fi
 
 GCLEAN=0        # want to run git reset ? (default: 1)
-KCLEAN=1        # want to run make clean ? (default: 1)
-KCONFIG=1       # want to copy and process conf file ? (default: 1)
+KCLEAN=0        # want to run make clean ? (default: 1)
+KCONFIG=0       # want to copy and process conf file ? (default: 1)
 KMCONFIG=0      # want a menu to add/remove stuff from .config ? (default: 0)
 KLCONFIG=0      # want to merge a lsmod file into .config ? (default: 0)
 KDCONFIG=0      # default config (testing)
 KPREPARE=0      # want to prepare ? (default: 1)
 KBUILD=1        # want to build ? :o) (default: 1)
-KDEBUG=0        # want your kernel to have debug symbols ? (default: 1)
-KVERBOSE=0      # want it to shut up ? (default: 1)
+KDEBUG=1        # want your kernel to have debug symbols ? (default: 1)
+KVERBOSE=1      # want it to shut up ? (default: 1)
 
-KRAMFS=1        # TARGET will be a KRAMFSSIZE GB tmpfs (default: 0)
+KRAMFS=0        # TARGET will be a KRAMFSSIZE GB tmpfs (default: 0)
 KRAMFSSIZE=13   # TARGET dir size in GB
-KRAMFSUMNT=1    # TARGET will be unmounted (default: 0)
+KRAMFSUMNT=0    # TARGET will be unmounted (default: 0)
 
 FILEDIR="$HOME/work/files/kernel"
 MAINDIR="$HOME/work/sources/kernel"
