eab52da11b8c84bb1cf68983008b69113daf5f24
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2019-10-07 09:46:28 -0300

    Mon  7 Oct 09:46:01 -03 2019

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 
+if [ $UID -ne 0 ]; then
+    # re-run as root
+
+    sudo $0
+    exit 0
+fi
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 04EE7237B7D453EC
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7638D0442B90D010
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C8CAB6595FDFF622
 
 flavor=$(lsb_release -c -s)
+myarch=$(uname -i)
+
+case $myarch in
+
+s390x|armv8l|aarch64)
 
 echo """## /etc/apt/sources.list
 
@@ -26,47 +38,74 @@ deb http://ddebs.ubuntu.com $flavor-proposed main restricted universe multiverse
 
 ## end of file""" | tee /etc/apt/sources.list
 
-apt-get update -y
-apt-get dist-upgrade -y
+;;
 
-sudo apt-get install -y gnupg
-sudo apt-get install -y gnupg1
-sudo apt-get install -y gnupg2
+*)
+echo """## /etc/apt/sources.list
 
-set -e
+deb http://br.archive.ubuntu.com/ubuntu/ $flavor main restricted universe multiverse
+deb http://br.archive.ubuntu.com/ubuntu/ $flavor-updates main restricted universe multiverse
+deb http://br.archive.ubuntu.com/ubuntu/ $flavor-proposed main restricted universe multiverse
+# deb http://br.archive.ubuntu.com/ubuntu/ $flavor-backports restricted universe multiverse
+# deb http://security.ubuntu.com/ubuntu/ $flavor-security main restricted universe multiverse
+
+deb-src http://br.archive.ubuntu.com/ubuntu/ $flavor main restricted universe multiverse
+deb-src http://br.archive.ubuntu.com/ubuntu/ $flavor-updates main restricted universe multiverse
+deb-src http://br.archive.ubuntu.com/ubuntu/ $flavor-proposed main restricted universe multiverse
+# deb-src http://br.archive.ubuntu.com/ubuntu/ $flavor-backports restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ $flavor-security main restricted universe multiverse
+
+deb http://ddebs.ubuntu.com $flavor main restricted universe multiverse
+deb http://ddebs.ubuntu.com $flavor-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $flavor-proposed main restricted universe multiverse
+
+## end of file""" | tee /etc/apt/sources.list
+;;
+
+esac
+
+apt update -y
+apt dist-upgrade -y
+
+# locales
+
+apt-get install -y locales
+echo """LANG=\"en_GB.UTF-8\"
+LANGUAGE=\"en_GB:en\"""" | tee /etc/default/locale
+echo """en_US.UTF-8 UTF-8
+en_GB.UTF-8 UTF-8""" | tee /etc/locale.gen
+locale-gen
+
+# gnupg
+
+apt install -y gnupg
+apt install -y gnupg1
+apt install -y gnupg2
 
 # http_proxy="http://172.16.0.1:3142/" ; export http_proxy ;
 # https_proxy="https://172.16.0.1:3142/" ; export https_proxy ;
 # ftp_proxy="ftp://172.16.0.1:3142/" ; export ftp_proxy ;
 
-PACKAGES="""\
-locales less vim sudo openssh-server bash-completion \
-wget rsync git bridge-utils vlan net-tools xterm ncurses-term \
-build-essential mtr-tiny iputils-arping lsof strace ltrace \
-iputils-ping iputils-tracepath dnsutils bridge-utils lsb-release \
-tcpdump curl openssh-server openssh-client gdebi-core git \
-devscripts gnupg vlan
-"""
+apt install -y locales less vim sudo openssh-server bash-completion
+apt install -y bridge-utils vlan ifupdown resolvconf net-tools
+apt install -y wget rsync git curl tcpdump
+apt install -y snapd squashfuse
+apt install -y xterm ncurses-term
+apt install -y mtr-tiny iputils-arping iputils-ping iputils-tracepath traceroute
+apt install -y dnsutils
+apt install -y openssh-client
+apt install -y gdebi-core
+apt install -y gtk2-engines gtk3-engines-breeze gnome-themes-standard
+apt install -y x11-xserver-utils
+apt install -y haveged
 
-apt-get install -y $PACKAGES
-
-echo en_US.UTF-8 > /etc/locale.gen
-locale-gen en_US.UTF-8
-
-set +e
+# users
 
 passwd -d root
 useradd -d /home/inaddy -m -s /bin/bash inaddy
 passwd -d inaddy
-
 echo root:root | chpasswd
 echo inaddy:inaddy | chpasswd
-
-groupdel dialout
-groupmod -g 20 -o inaddy
-usermod -u 501 -g 20 -o inaddy
-#chown -R inaddy:inaddy /home/inaddy
-
 usermod -a -G root inaddy
 usermod -a -G sudo inaddy
 usermod -a -G shadow inaddy
@@ -80,13 +119,48 @@ usermod -a -G libvirtd inaddy
 usermod -a -G libvirt-qemu inaddy
 usermod -a -G kvm inaddy
 
-set -e
-
 echo """## /etc/apt/apt.conf
 
 # Acquire::http::Proxy \"http://172.16.0.1:3142/\";
 
 ## end of file""" | tee /etc/apt/apt.conf
+
+echo """## /etc/network/interfaces
+
+auto lo
+iface lo inet loopback
+    dns-nameservers 8.8.8.8
+    dns-nameservers 8.8.4.4
+    post-up sysctl -w net.ipv4.conf.all.forwarding=0
+    post-up sysctl -w net.ipv4.conf.default.forwarding=0
+    post-up sysctl -w net.ipv6.conf.all.disable_ipv6=1
+    post-up sysctl -w net.ipv6.conf.default.disable_ipv6=1
+    pre-down sysctl -w net.ipv4.conf.all.forwarding=0
+    pre-down sysctl -w net.ipv4.conf.default.forwarding=0
+    pre-down sysctl -w net.ipv6.conf.all.disable_ipv6=0
+    pre-down sysctl -w net.ipv6.conf.default.disable_ipv6=0
+
+iface eth0 inet manual
+    mtu 1472
+    post-up sysctl -w net.ipv6.conf.eth0.disable_ipv6=1
+    post-up sysctl -w net.ipv4.conf.eth0.forwarding=0
+    pre-down sysctl -w net.ipv4.conf.eth0.forwarding=0
+    pre-down sysctl -w net.ipv6.conf.eth0.disable_ipv6=0
+
+auto bridge0
+iface bridge0 inet dhcp
+    bridge_ports eth0
+    bridge_waitport 0
+    bridge_fd 0
+    bridge_stp off
+    bridge_maxwait 0
+    mtu 1472
+    post-up sysctl -w net.ipv6.conf.bridge0.disable_ipv6=1
+    post-up sysctl -w net.ipv4.conf.bridge0.forwarding=0
+    pre-down sysctl -w net.ipv4.conf.bridge0.forwarding=0
+    pre-down sysctl -w net.ipv6.conf.bridge0.disable_ipv6=0
+
+## end of file""" | tee /etc/network/interfaces
 
 echo """## /etc/sudoers
 
@@ -107,42 +181,12 @@ inaddy ALL=(ALL) NOPASSWD: ALL
 ## end of file""" | tee /etc/sudoers
 
 echo """## /etc/modules
+nbd
 ## end of file""" | tee /etc/modules
 
 echo """## /etc/initramfs-tools/modules
+nbd
 ## end of file""" | tee /etc/initramfs-tools/modules
-
-
-echo """## /etc/network/interfaces
-
-auto lo
-iface lo inet loopback
-    dns-nameservers 192.168.100.1
-    post-up sysctl -w net.ipv4.conf.all.forwarding=0
-    post-up sysctl -w net.ipv4.conf.default.forwarding=0
-    pre-down sysctl -w net.ipv4.conf.all.forwarding=0
-    pre-down sysctl -w net.ipv4.conf.default.forwarding=0
-
-iface eth0 inet manual
-    post-up sysctl -w net.ipv6.conf.eth0.disable_ipv6=1
-    post-up sysctl -w net.ipv4.conf.eth0.forwarding=0
-    pre-down sysctl -w net.ipv4.conf.eth0.forwarding=0
-    pre-down sysctl -w net.ipv6.conf.eth0.disable_ipv6=0
-
-auto bridge0
-iface bridge0 inet dhcp
-    bridge_ports eth0
-    bridge_waitport 0
-    bridge_fd 0
-    bridge_stp off
-    bridge_maxwait 0
-    hwaddress 52:54:00:BC:17:80
-    post-up sysctl -w net.ipv6.conf.bridge0.disable_ipv6=1
-    post-up sysctl -w net.ipv4.conf.bridge0.forwarding=0
-    pre-down sysctl -w net.ipv4.conf.bridge0.forwarding=0
-    pre-down sysctl -w net.ipv6.conf.bridge0.disable_ipv6=0
-
-## end of file""" | tee /etc/network/interfaces
 
 echo "" | tee /etc/motd
 echo "" | tee /etc/issue
@@ -197,7 +241,7 @@ PermitTTY yes
 PrintMotd no
 PrintLastLog no
 TCPKeepAlive yes
-UseLogin no
+# UseLogin no
 PermitUserEnvironment no
 Compression delayed
 #ClientAliveInterval 0
@@ -315,19 +359,18 @@ session    optional   pam_keyinit.so force revoke
 
 ## end of file """ | tee /etc/pam.d/login
 
-ARCH=$(uname -r | cut -d'-' -f3)
+apt install -y libvirt0 libvirt-bin
+apt install -y libvirt-clients libvirt-daemon libvirt-daemon-system
 
-apt-get install -y linux-image-generic linux-headers-generic
-apt-get build-dep -y hello
-
-set +e
-
-apt-get install -y libvirt0 libvirt-bin
-apt-get install -y libvirt-clients libvirt-daemon libvirt-daemon-system
-apt-get install -y qemu-system-x86 qemu-user-static qemu-utils qemu-kvm
-apt-get install -y qemu-block-extra
-
-set -e
+if [ "$myarch" == "aarch64" ] || [ "$myarch" == "armv8l" ]; then
+    apt install -y qemu-system-arm
+elif [ "$myarch" == "s390x" ]; then
+    apt install -y qemu-system-s390x
+else
+    apt install qemu-system-x86
+fi
+apt install -y qemu-user-static qemu-utils qemu-kvm
+apt install -y qemu-block-extra
 
 echo """## /etc/libvirt/qemu.conf
 
@@ -338,3 +381,88 @@ clear_emulator_capabilities = 0
 seccomp_sandbox = 0
 
 ## end of file """ | tee /etc/libvirt/qemu.conf
+
+## final stage
+
+# cloud-init removal
+
+apt remove --purge -y cloud-guest-utils
+apt remove --purge -y cloud-init
+apt remove --purge -y cloud-initramfs-copymods
+apt remove --purge -y cloud-initramfs-dyn-netconf
+
+# leftovers
+
+apt remove --purge -y unattended-upgrades
+apt remove --purge -y irqbalance
+apt remove --purge -y open-iscsi
+apt remove --purge -y multipath-tools
+apt remove --purge -y thermald
+apt remove --purge -y pollinate
+apt remove --purge -y ufw
+
+# services
+
+systemctl disable systemd-resolved
+systemctl disable accounts-daemon.service
+systemctl disable apparmor.service
+systemctl disable apport-autoreport.path
+systemctl disable apport-forward.socket
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+systemctl disable dbus-org.freedesktop.thermald.service
+systemctl disable dbus-org.freedesktop.timesync1.service
+systemctl disable ebtables.service
+systemctl disable irqbalance.service
+systemctl disable iscsi.service
+systemctl disable iscsid.socket
+systemctl disable motd-news.timer
+systemctl disable multipath-tools.service
+systemctl disable multipathd.service
+systemctl disable multipathd.socket
+systemctl disable open-iscsi.service
+systemctl disable pollinate.service
+systemctl disable rsync.service
+systemctl disable thermald.service
+systemctl disable ufw.service
+systemctl disable unattended-upgrades.service
+systemctl disable systemd-networkd-wait-online.service;
+
+systemctl disable lvm2-lvmpolld.socket
+systemctl disable lvm2-monitor.service
+systemctl disable man-db.timer
+systemctl disable networkd-dispatcher.service
+systemctl disable secureboot-db.service
+systemctl disable libvirtd.service
+systemctl disable libvirt-guests.service
+systemctl disable qemu-kvm.service
+systemctl disable virtlockd.socket
+systemctl disable virtlogd.socket
+systemctl disable virtlockd-admin.socket
+systemctl disable virtlogd-admin.socket
+systemctl disable ureadahead.service
+systemctl disable lvm2-lvmetad.socket
+systemctl disable e2scrub_all.timer
+systemctl disable e2scrub_reap.service
+
+# development
+
+apt install -y linux-headers-generic
+apt install -y build-essential
+apt install -y devscripts
+apt install -y ubuntu-dev-tools
+
+apt-get build-dep -y hello
+apt-get build-dep -y linux-image-$(uname -r)
+
+# snapd
+
+apt install -y snapd
+snap install hello-world
+snap install --edge --clasic git-ubuntu
+
+# perf tools
+
+apt install -y htop atop iotop
+systemctl disable atop.service
+systemctl disable atopacct.service
