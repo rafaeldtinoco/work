938bb1b91d011ff0336493dc6b170b94325064ae
Rafael David Tinoco <rafaeldtinoco@ubuntu.com>
2020-03-15 05:50:24 +0000

    source/upstream: improving gitupdate logic

--- sources/upstream/.gitupdate.sh
+++ sources/upstream/.gitupdate.sh
@@ -21,15 +21,38 @@ for dir in $dirs; do
 
     echo "---- $(basename $(pwd) | tr [:lower:] [:upper:]) ----"
 
-    origin=$(git remote -v | grep fetch | head -1 | awk '{print $1}')
+    # clean and fetch origin
 
+    git clean -fd ; git reset --hard
+    origin=$(git remote -v | grep fetch | head -1 | awk '{print $1}')
     git fetch $origin -a --tags
 
-    branch=$(git branch --no-color -al | grep HEAD | awk '{print $3}')
+    # create a temporary branch with remote head
 
-    git clean -fd ; git reset --hard ; git checkout $branch -b temporary
-    git branch -D $(basename $branch)
-    git checkout $branch -b $(basename $branch)
+    headptr=$(git branch --no-color -al | sed 's:\*::g' | grep HEAD | awk '{print $3}')
+    git checkout temporary 2>&1 > /dev/null 2>&1 || git checkout $headptr -b temporary
+
+    # delete all branches
+
+    branches=$(git branch --no-color -l | sed 's:\*::g' | \
+        grep -v "ubuntu/devel$" | grep -v "temporary" | \
+        awk '{print $1}' | xargs)
+
+    for branch in $branches; do
+        git branch -D $branch
+    done
+
+    # delete all remotes but origin
+
+    remotes=$(git remote -v | grep fetch | grep -v origin | awk '{print $1}' | xargs)
+
+    for remote in $remotes; do
+        git remote remove $remote
+    done
+
+    # checkout remote head as default
+
+    git checkout $headptr -b $(basename $headptr)
     git branch -D temporary
 
     cd $now
